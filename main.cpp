d56f2e23e398c3b672cc1f6f81e1f536c02a0c4c
lucascogrossi <lucasmachado292@gmail.com>
2024-07-06 20:13:07 -0300

    first commit

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 int main() {
 
     // Image
-    int image_width = 256;
-    int image_height = 256;
+    int image_width = 1920;
+    int image_height = 1080;
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
