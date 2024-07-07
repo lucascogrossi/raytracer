1b1b843ee60af86278044b5d8f3ba8369e5bfa5f
lucascogrossi <lucasmachado292@gmail.com>
2024-07-07 15:53:10 -0300

    chapter 3.1

--- main.cpp
+++ main.cpp
@@ -1,25 +1,23 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
 
-    // Image
-    int image_width = 1920;
-    int image_height = 1080;
+	// Image
+	int image_width = 256;
+	int image_height = 256;
 
-    // Render
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	// Render
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) {
-        for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
-        }
-    }
+	for (int j = 0; j < image_height; ++j) {
+		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+		for (int i = 0; i < image_width; ++i) {
+			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+			write_color(std::cout, pixel_color);
+		}
+	}
+	std::clog << "\rDone.                 \n";
 }
